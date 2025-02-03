e87b20a200038f6e4031b4052d957c2497b55f29
TNTStudiosn <tntstudiosn@gmail.com>
2025-02-03 11:29:50 -0600

    Initial commit

---
+++ src/client/java/com/TNTStudios/fristjoin/client/FristjoinClient.java
@@ -0,0 +1,10 @@
+package com.TNTStudios.fristjoin.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FristjoinClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/TNTStudios/fristjoin/Fristjoin.java
@@ -0,0 +1,10 @@
+package com.TNTStudios.fristjoin;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Fristjoin implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
